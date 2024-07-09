8c7caefc82465cf150df247cb0c787f9d3d07c9b
officialksolomon <solomonuche42@gmail.com>
2024-07-09 10:12:46 +0100

    update project urls pattern

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,14 @@ urlpatterns = [
     path("auth/logoutall/", knox_views.LogoutAllView.as_view(), name="logoutall"),
 ]
 
+urlpatterns += [
+    # auth
+    path("auth/register", RegisterView.as_view(), name="register"),
+    path("auth/login", LoginView.as_view(), name="login"),
+    path("auth/logout", knox_views.LogoutView.as_view(), name="logout"),
+    path("auth/logoutall", knox_views.LogoutAllView.as_view(), name="logoutall"),
+]
+
 if settings.DEBUG:
     import debug_toolbar
 
