02fe26178dded34c989fa54d6b21414b05b4f6fe
officialksolomon <solomonuche42@gmail.com>
2024-07-09 09:39:24 +0100

    RegisterSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
+
 from accounts.models import CustomUser
-from api.models import  Organisation
+from api.models import Organisation
+
+from api.models import Organisation
+
 
 class CamelCaseSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
@@ -8,26 +12,29 @@ class CamelCaseSerializer(serializers.ModelSerializer):
         return {self.to_camel_case(key): value for key, value in ret.items()}
 
     def to_camel_case(self, snake_str):
-        components = snake_str.split('_')
-        return components[0] + ''.join(x.title() for x in components[1:])
+        components = snake_str.split("_")
+        return components[0] + "".join(x.title() for x in components[1:])
+
 
 class UserSerializer(CamelCaseSerializer):
     class Meta:
         model = CustomUser
-        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone']
+        fields = ["user_id", "first_name", "last_name", "email", "phone"]
+
 
 class RegisterSerializer(CamelCaseSerializer):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'email', 'password', 'phone']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ["user_id", "first_name", "last_name", "email", "password", "phone"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
-        organisation = Organisation.objects.create(name=f"{user.first_name}'s Organisation", description='')
+        organisation = Organisation.objects.create(
+            name=f"{user.first_name}'s Organisation", description=""
+        )
         organisation.users.set([user])
         return user
-
 
 
 class LoginSerializer(serializers.Serializer):
@@ -38,4 +45,4 @@ class LoginSerializer(serializers.Serializer):
 class OrganisationSerializer(CamelCaseSerializer):
     class Meta:
         model = Organisation
-        fields = ['org_id', 'name', 'description']
+        fields = ["org_id", "name", "description"]
